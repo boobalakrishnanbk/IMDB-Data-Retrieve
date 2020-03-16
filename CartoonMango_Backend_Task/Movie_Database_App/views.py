3e5034bd42ad87e467c3e436974382b0fa34d80e
boobalakrishnanbk <62205406+boobalakrishnanbk@users.noreply.github.com>
2020-03-16 15:54:43 +0530

    Update views.py

--- CartoonMango_Backend_Task/Movie_Database_App/views.py
+++ CartoonMango_Backend_Task/Movie_Database_App/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import URL_GET
 from imdb import IMDb
+from django.views import generic
 from .models import IMDB_Database
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+# from json import JsonResponse
+
+from .serializers import IMDbSerializer
 # Create your views here.
 
-def home(response):
+def home(request):
     count = 0
-    if response.method == 'POST':
-        url_form = URL_GET(response.POST)
+    res = None
+    if request.method == 'POST':
+        url_form = URL_GET(request.POST)
         if url_form.is_valid():
             url = url_form.cleaned_data["url"]
             temp = url.split('/')
@@ -17,6 +25,7 @@ def home(response):
 
             imdb_instance = IMDb()
             movie_details = imdb_instance.get_movie(int(final_url_id))
+
             movie_title = movie_details['title']
             movie_summary = movie_details['plot outline']
 
@@ -24,16 +33,19 @@ def home(response):
             movie_writers = ""
             for sample in writers:
                 movie_writers += str(sample)+','
+            print(movie_writers)
 
             directors = movie_details['directors']
             movie_directors = ""
             for sample in directors:
                 movie_directors += sample['name']+','
+            # print(movie_directors)
 
             cast = movie_details['cast']
             movie_cast = ""
             for sample in cast:
                 movie_cast +=sample['name']+','
+            # print(movie_cast)
 
             movie_rating = movie_details['rating']
 
@@ -41,6 +53,12 @@ def home(response):
             database_instance.save()
 
             count = 1
-            return render(response,'home.html',{"title":movie_title,"summary":movie_summary,"directors":movie_directors,"writers":movie_writers,"stars":movie_cast,"rating":movie_rating,"image_url":movie_details['full-size cover url'],"output":count,"url_get":url_form})    else:
+            # print(database_instance[2])
+            movie = IMDB_Database.objects.all().values()
+            movie_list = list(movie)
+            # content = {'movie': movie}
+            return JsonResponse(movie_list, safe=False)
+            # return render(request,'home.html',{"title":movie_title,"summary":movie_summary,"directors":movie_directors,"writers":movie_writers,"stars":movie_cast,"rating":movie_rating,"image_url":movie_details['full-size cover url'],"output":count,"get_url":url_form, 'res':res})
+    else:
         url_form = URL_GET()
-    return render(response,'home.html',{"url_get":url_form,"output":count})
+    return render(request,'home.html',{"get_url":url_form,"output":count, 'res':res})
