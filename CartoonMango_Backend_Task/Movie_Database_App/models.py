9464a4e76de3651767add5961722f7b23703ce49
boobalakrishnanbk <62205406+boobalakrishnanbk@users.noreply.github.com>
2020-04-08 21:58:50 +0530

    Update models.py

--- CartoonMango_Backend_Task/Movie_Database_App/models.py
+++ CartoonMango_Backend_Task/Movie_Database_App/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django_mysql.models import ListCharField
+from django.db.models import CharField
 
 # Create your models here.
 
 class IMDB_Database(models.Model):
     title = models.CharField(max_length=200)
-    plot_summary = models.CharField(max_length=500)
-    directors = models.CharField(max_length=5000)
-    writers = models.CharField(max_length=5000)
-    stars = models.CharField(max_length=5000)
+    plot_summary = models.CharField(max_length=50000)
+    directors =  ListCharField(base_field=CharField(max_length=20),size=50,max_length=(50 * 21))
+    writers = ListCharField(base_field=CharField(max_length=20),size=50,max_length=(50 * 21))
+    stars = ListCharField(base_field=CharField(max_length=30),size=50,max_length=(50 * 31))
     rating = models.FloatField(max_length=10)
 
     def __str__(self):
