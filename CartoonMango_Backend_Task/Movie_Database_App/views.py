759ca1912820e7275187af18e5239b32200633d2
boobalakrishnanbk <62205406+boobalakrishnanbk@users.noreply.github.com>
2020-04-08 21:59:41 +0530

    Update views.py

--- CartoonMango_Backend_Task/Movie_Database_App/views.py
+++ CartoonMango_Backend_Task/Movie_Database_App/views.py
@@ -7,8 +7,6 @@ from .models import IMDB_Database
 # Create your views here.
 
 def home(request):
-    count = 0
-    res = None
     if request.method == 'POST':
         url_form = URL_GET(request.POST)
         if url_form.is_valid():
@@ -24,22 +22,23 @@ def home(request):
             movie_summary = movie_details['plot outline']
 
             writers = movie_details['writers']
-            movie_writers = ""
+            movie_writers = []
             for sample in writers:
-                movie_writers += str(sample)+','
-            # print(movie_writers)
+                if str(sample)=='':
+                    pass
+                else:
+                    movie_writers.append(str(sample))
+            print(movie_writers)
 
             directors = movie_details['directors']
-            movie_directors = ""
+            movie_directors = []
             for sample in directors:
-                movie_directors += sample['name']+','
-            # print(movie_directors)
+                movie_directors.append(sample['name'])
 
             cast = movie_details['cast']
-            movie_cast = ""
+            movie_cast = []
             for sample in cast:
-                movie_cast +=sample['name']+','
-            # print(movie_cast)
+                movie_cast.append(sample['name'])
 
             movie_rating = movie_details['rating']
 
@@ -49,12 +48,10 @@ def home(request):
                 database_instance = IMDB_Database(title=movie_title,plot_summary=movie_summary,directors=movie_directors,writers=movie_writers,stars=movie_cast,rating=movie_rating)
                 database_instance.save()
 
-            count = 1
-
             movie = IMDB_Database.objects.filter(title=movie_title).values()
             movie_list = list(movie)
+            return HttpResponse(movie_list)
 
-            return render(request,'home.html',{"title":movie_title,"summary":movie_summary,"directors":movie_directors,"writers":movie_writers,"stars":movie_cast,"rating":movie_rating,"image_url":movie_details['full-size cover url'],"output":count,"get_url":url_form, 'res':movie_list})
     else:
         url_form = URL_GET()
-    return render(request,'home.html',{"get_url":url_form,"output":count, 'res':res})
+    return render(request,'home.html',{"get_url":url_form})
